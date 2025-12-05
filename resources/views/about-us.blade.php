c92ab1d0035f28c8844fb658fd554adc07a5c6a5
ratanswarnkar <ratanswarnkar7052@gmail.com>
2025-12-05 23:18:54 +0530

    fgd

--- resources/views/about-us.blade.php
+++ resources/views/about-us.blade.php
@@ -269,7 +269,7 @@ Maybach Homes Property Group India – Crafting the future of luxury real estate
     <div class="team-row row-4">
       <div class="member">
          <img src="images/cattly.jpeg" alt="Client Relationship Manager">
-        <h3>Dlna Sharma </h3>
+        <h3>Dina Sharma </h3>
         <p>English <br> Real Estate agent</p>
       </div>
       <div class="member">
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1310,7 +1310,7 @@ document.addEventListener("DOMContentLoaded", function() {
     <div class="team-row row-4">
       <div class="member">
         <img src="images/cattly.jpeg" alt="Client Relationship Manager">
-        <h3>Dlna Sharma </h3>
+        <h3>Dina Sharma </h3>
         <p>English <br> Real Estate agent</p>
       </div>
       <div class="member">
